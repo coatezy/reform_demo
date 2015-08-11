e0cf05f8559aebbb43bc846570cb15450460ee92
Yury Velikanau <yury.velikanau@gmail.com>
2015-08-10 20:35:01 -0700

    Or you could do this, however w/o custom coercion it is not possible to get rid of `def category_ids=(value)`

--- app/forms/post_form.rb
+++ app/forms/post_form.rb
@@ -1,14 +1,17 @@
+require "reform/form/coercion"
+
 class PostForm < Reform::Form
   include ActiveModel::ModelReflections
+  include Reform::Form::Coercion
 
   model :post
 
   properties :title, :body, validates: {presence: true} 
-  property :category_ids
+  property :category_ids, type: Array[Integer]
 
   validates :category_ids, length: {minimum: 2, message: 'Select at least two categories'}
 
   def category_ids=(value)
-    super value.select(&:present?).map(&:to_i)
+    super value.select(&:present?)
   end
 end
